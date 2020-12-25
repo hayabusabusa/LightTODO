074a621c7e2c200d069d04034a76561241368c09
hayabusabusa <hayabusabusaSY@gmail.com>
2020-12-25 13:43:11 +0900

    refactor: remove default template view controllers and show todos view controller as root view controller

---
+++ LightTODO/View/Todos/TodosViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TodosViewController.swift
+//  LightTODO
+//
+//  Created by 山田隼也 on 2020/12/25.
+//
+
+import UIKit
+
+final class TodosViewController: UIViewController {
+    
+    // MARK: IBOutlet
+    
+    // MARK: Properties
+    
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
