125234257e9bb6f552a8a665dbc821da7016baae
hayabusabusa <hayabusabusaSY@gmail.com>
2020-12-25 22:01:15 +0900

    test: add decoding entity tests

--- LightTODOTests/Data/Entity/TodosTests.swift
+++ LightTODOTests/Data/Entity/TodosTests.swift
@@ -6,10 +6,65 @@
 //
 
 import XCTest
+@testable import LightTODO
 
 class TodosTests: XCTestCase {
 
-    func test() {
+    func testDecodingTodoEntity() {
+        let json = """
+        {
+            "title": "TEST",
+            "detail": "TEST",
+            "isCompleted": true
+        }
+        """.data(using: .utf8)!
         
+        let todo = try? JSONDecoder().decode(Todo.self, from: json)
+        
+        XCTAssertNotNil(todo)
+        XCTAssertEqual(todo?.title, "TEST")
+        XCTAssertEqual(todo?.detail, "TEST")
+        XCTAssertEqual(todo?.isCompleted, true)
+    }
+    
+    func testDecodingTodosEntity() {
+        let json = """
+        {
+            "items": [
+                {
+                    "title": "TEST",
+                    "detail": "TEST",
+                    "isCompleted": true
+                },
+                {
+                    "title": "TEST",
+                    "detail": "TEST",
+                    "isCompleted": true
+                }
+            ]
+        }
+        """.data(using: .utf8)!
+        
+        let todos = try? JSONDecoder().decode(Todos.self, from: json)
+        
+        XCTAssertNotNil(todos)
+        XCTAssertEqual(todos?.items.count, 2)
+    }
+    
+    func testEncodingTodoEntity() {
+        let todo = Todo(title: "TEST", detail: "TEST", isCompleted: true)
+        let data = try? JSONEncoder().encode(todo)
+        
+        XCTAssertNotNil(data)
+    }
+    
+    func testEncodingTodosEntity() {
+        let todos = Todos(items: [
+            Todo(title: "TEST", detail: "TEST", isCompleted: true),
+            Todo(title: "TEST", detail: "TEST", isCompleted: false)
+        ])
+        let data = try? JSONEncoder().encode(todos)
+        
+        XCTAssertNotNil(data)
     }
 }
