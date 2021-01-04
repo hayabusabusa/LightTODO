4c83fd29b77ffa1257bc1e756d3995875e9babb4
hayabusabusa <hayabusabusaSY@gmail.com>
2021-01-04 11:44:24 +0900

    test: add todos model toggle todo test

--- LightTODOTests/Model/Todos/TodosModelTests.swift
+++ LightTODOTests/Model/Todos/TodosModelTests.swift
@@ -22,4 +22,29 @@ class TodosModelTests: XCTestCase {
         
         XCTAssertEqual(receiver.todos.count, 1)
     }
+    
+    func testToggleTodo() {
+        let todos = Todos(items: [
+            Todo(id: "ID", title: "TEST", detail: "TEST", isCompleted: false)
+        ])
+        let model = TodosModel(provider: MockUserDefaultsProvider<Todos>(stored: todos))
+        let receiver = TodosModelReceiver()
+        
+        model.delegate = receiver
+        model.getTodos()
+        
+        XCTContext.runActivity(named: "存在しない ID の場合は何も変化していないことを確認") { _ in
+            model.toggleTodo(of: "TEST")
+            model.getTodos()
+            
+            XCTAssertEqual(receiver.todos.first?.isCompleted, false)
+        }
+        
+        XCTContext.runActivity(named: "存在する ID の場合は `isCompleted` が `true` になることを確認") { _ in
+            model.toggleTodo(of: "ID")
+            model.getTodos()
+            
+            XCTAssertEqual(receiver.todos.first?.isCompleted, true)
+        }
+    }
 }
